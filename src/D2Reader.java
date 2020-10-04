f4e93a548165be9577b19de7473a6e1d540d7251
Collin <collin.harmon1@gmail.com>
2020-10-03 21:22:42 -0500

    Initial commit before removing possible needed dependencies

---
+++ src/D2Reader.java
@@ -0,0 +1,9 @@
+import character.*;
+
+public class D2Reader {
+    public static void main(String[] args){
+        String fileName = "C:\\Users\\colli\\d2beyondcompare\\save_HC_progress_9_12_2020\\Rava.d2s";
+        D2Character d2Char = new D2Character(fileName);
+        System.out.println("Hello world!");
+    }
+}
